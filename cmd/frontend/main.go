d73141810f9991625b29c4ae0bbaba26fc6eefcd
KoFeeBriAN <coffeetk2017@gmail.com>
2021-08-06 15:30:53 +0700

    comment, refactor: no need files

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -75,45 +75,57 @@ func main() {
 func (f *FrontEnd) Start(clientAPIListenAddr string, storageAPIListenAddr string, storageTimeout uint8) error {
 	result := new(FrontEnd)
 
+	// Dial to Storage service
 	rpcClient, rpcErr := rpc.DialHTTP("tcp", storageAPIListenAddr)
 	if rpcErr != nil {
 		return errors.New("cannot established connection with Storage server")
 	}
 
+	// Assign Storage Client to struct variable
 	result.rpcClient = rpcClient
 
+	// Init gRPC server
 	grpcServer := grpc.NewServer()
+	// Register Frontend struct to the server
 	pb.RegisterFrontendServer(grpcServer, result)
 
+	// Listen to port
 	lis, err := net.Listen("tcp", clientAPIListenAddr)
 	if err != nil {
 		return fmt.Errorf("failed to listen: %s", clientAPIListenAddr)
 	}
 
-	// start serving
+	// Start gRPC server (Blocking)
 	log.Println("serving on port:", clientAPIListenAddr)
 	log.Fatal(grpcServer.Serve(lis))
 
+	// If code not blocking, mean Fail to serve the server
 	return errors.New("frontend Fail")
 }
 
+/** 
+ * TODO: Handle Client Timeout => Cancel the action 
+ * TODO: Handle Retry Request on Failed
+ */
+// Function define by Protobuf file
 func (f *FrontEnd) HandleGet(
 	ctx context.Context, 
 	req *pb.FrontendGetRequest,
 ) (*pb.FrontendGetReponse, error) {
 
 	// convert to storage args
-	storageArgs := StorageGet{req.Key}
-	storageReply := new(StorageGetResult)
-	funcCall := f.rpcClient.Go("Storage.StorageGet", storageArgs, &storageReply, nil)
-	replyCall := <-funcCall.Done
-
+	storageArgs := StorageGet{req.Key}	// Storage Request Struct
+	storageReply := new(StorageGetResult)	// Storage Response Struct
+	funcCall := f.rpcClient.Go("Storage.StorageGet", storageArgs, &storageReply, nil) // Call Storage Func
+	replyCall := <-funcCall.Done // Wait for response from Storage
 	if replyCall.Error != nil {
 		return nil, errors.New("FE to Storage fail")
 	}
 
+	// DEBUG: Print Value of Key gotten from Storage 
 	log.Printf("OpId: %d Get value %s from %s", req.OpId, *storageReply.Value, req.Key)
 
+	// Return Result to Protobuf
 	return &pb.FrontendGetReponse{
 		OpId: req.OpId,
 		StorageFail: false,
@@ -121,16 +133,22 @@ func (f *FrontEnd) HandleGet(
 	}, nil
 }
 
+/** 
+ * TODO: Handle Client Timeout => Cancel the action 
+ * TODO: Handle Retry Request on Failed
+ */
+// Function define by Protobuf file
 func (f *FrontEnd) HandlePut(
 	ctx context.Context, 
 	req *pb.FrontendPutRequest, 
 	) (*pb.FrontendPutReponse, error) {
+	// Same as Get
 	storageArgs := StoragePut{req.Key, req.Value, 0}
 	storageReply := new(string)
 	funcCall := f.rpcClient.Go("Storage.StoragePut", storageArgs, &storageReply, nil)
 	replyCall := <-funcCall.Done
-
 	if replyCall.Error != nil {
+		// Return Empty Struct w/ Error
 		return &pb.FrontendPutReponse{
 			OpId: req.OpId,
 			StorageFail: true,
@@ -138,8 +156,10 @@ func (f *FrontEnd) HandlePut(
 		}, errors.New("FE to Storage fail")
 	}
 
+	// DEBUG: Print Value
 	log.Printf("OpId: %d Put value %s to %s", req.OpId, *storageReply, req.Key)
 
+	// Return Result to Protobuf
 	return &pb.FrontendPutReponse{
 		OpId: req.OpId,
 		StorageFail: false,
